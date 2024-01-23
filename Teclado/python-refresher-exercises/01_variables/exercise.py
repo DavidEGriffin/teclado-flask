fecd3f7f690ed0b0f259308c8b76dfbba0f32445
DavidEGriffin <115192861+DavidEGriffin@users.noreply.github.com>
2024-01-23 13:01:54 -0500

    Add python refresher exercises

---
+++ Teclado/python-refresher-exercises/01_variables/exercise.py
@@ -0,0 +1,7 @@
+# Create two variables, var1 and var2, both with the same value.
+var1 = 13
+var2 = var1
+# Create two variables, num1 and num2, which multiply together to give 16.
+num1 = 4
+num2 = num1
+print("4 times 4 equals "+str(num1*num2))
---
+++ Teclado/python-refresher-exercises/extra/first_app.py
@@ -0,0 +1,5 @@
+# Request user age and print the conversion to months
+
+years = int(input("How old are you? "))
+months = years * 12
+print(f"You are {months} months old (plus change)!")
