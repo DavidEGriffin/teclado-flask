f147c18f8897e789805de14b02ad4fd4dfc2ccf4
DavidEGriffin <davidelliotgriffin@gmail.com>
2024-01-23 16:37:18 -0500

    Complete 07_classmethod_staticmethod

--- Teclado/python-refresher-exercises/07_classmethod_staticmethod/exercise.py
+++ Teclado/python-refresher-exercises/07_classmethod_staticmethod/exercise.py
@@ -4,18 +4,28 @@ class Store:
         self.items = []
 
     def add_item(self, name, price):
-        self.items.append({"name": name, "price": price})
+        self.items.append({
+            'name': name,
+            'price': price
+        })
 
     def stock_price(self):
         total = 0
         for item in self.items:
-            total += item["price"]
+            total += item['price']
         return total
 
     @classmethod
     def franchise(cls, store):
-        return cls(store.name + " - franchise")
+        # Return another store, with the same name as the argument's name, plus " - franchise"
+        return Store(name = store + " - franchise")
 
     @staticmethod
     def store_details(store):
-        return store.name + ", total stock price: " + str(store.stock_price())
+        # Return a string representing the argument
+        # It should be in the format 'NAME, total stock price: TOTAL'
+        name = store.name
+        total = 0
+        for item in store.items:
+            total += item["price"]
+        return f"{name}, total stock price: {total}"
